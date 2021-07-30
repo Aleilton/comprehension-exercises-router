4f47099e0afda57e0a4b8a01d91ee33fdfb53e9d
Aleilton Chavenco <aleilton@gmail.com>
2021-07-30 19:17:32 -0300

    Requisitos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/users" >Users</Link>
+        <Link to="/strict-access" >Strict Access</Link>
+          <Switch>
+          <Route
+            path="/strict-access"
+            render={ () => (
+              <StrictAccess user={ { username: 'joao', password: '1234' } } />
+            )}
+          />
+                <Route
+                  path="/users/:id"
+                  render={(props) => <Users {...props} greetingsMessage={"Good Morning"} /> } 
+                />
+                <Route path="/about" component={About} />
+                <Route exact path="/" component={Home} />
+          </Switch>
+      </BrowserRouter>
     );
   }
 }
